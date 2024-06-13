c37981f68a60942d718b176132027a6202a31a60
Daniel Kovacs <info@kovix.info>
2024-06-14 00:38:45 +0200

    Display functionality

--- private/classes/display/display.php
+++ private/classes/display/display.php
@@ -14,6 +14,47 @@ class display {
 
     public function  run()
     {
+
+        $thead = "";
+        foreach (LABELS as $label) {
+            $thead .= "<th scope=\"col\">{$label}</th>";
+        }
+
+        $thead .= "<th>&nbsp;</th>";
+
+        $body = "";
+        for($i = 0;$i < count($this->data);$i++) {
+            $body .= "
+                <tr>
+            ";
+            foreach ($this->data[$i] as $value) {
+                $body .= "<td>{$value}</td>";
+            }
+            $body .= "
+                    <td>
+                        <a href=\"#\" class=\"text-primary me-2\">Szerk.</a>
+                        <a href=\"index.php?P=delete&idx={$i}\" onclick=\"return confirm('Biztosan törölni szeretné ezt az elemet?');\" class=\"text-danger\">Del.</a>
+                    </td>
+                </tr>
+            ";
+        }
+
+        $retval = "
+            <div class=\"table-responsive\">
+                <table class=\"table table-striped\">
+                    <thead>
+                        <tr>
+                            {$thead}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {$body}
+                    </tbody>
+                </table> 
+            </div>     
+        ";
+
+        return $retval;
     }
 
 }
